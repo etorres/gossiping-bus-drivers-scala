fbe4ab6bedef81789b74405cb8cba6d8d580312d
Erik Torres <erik.torres@lifullconnect.com>
2019-12-13 23:32:56 +0100

    :art: Add custom matcher to assert that gossips are merged

--- src/test/scala/es/eriktorr/katas/GossipsExchangerSpec.scala
+++ src/test/scala/es/eriktorr/katas/GossipsExchangerSpec.scala
@@ -1,20 +1,29 @@
 package es.eriktorr.katas
 
-class GossipsExchangerSpec extends UnitSpec {
+import org.scalatest.matchers.{MatchResult, Matcher}
 
-  private val BusRoute1: BusRoute = BusRoute(Seq(1, 2, 3))
-  private val BusRoute2: BusRoute = BusRoute(Seq(5, 4, 3))
+class GossipsExchangerSpec extends UnitSpec with DriverMatchers {
 
-  private val Gossip1: Gossip = Gossip(1)
-  private val Gossip2: Gossip = Gossip(2)
-
-  "Gossips exchanger" should "merge gossips from all drivers at a given minute" in new GossipsExchanger {
-    val drivers = Seq(
-      Driver(BusRoute1, Set(Gossip1)),
-      Driver(BusRoute2, Set(Gossip2)))
-
-    driversAfterExchangingGossipsAt(3, drivers) shouldBe Seq(
-      Driver(BusRoute1, Set(Gossip1, Gossip2)),
-      Driver(BusRoute2, Set(Gossip1, Gossip2)))
+  "Gossips exchanger" should "merge all gossips at current minute" in new GossipsExchanger {
+    driversAfterExchangingGossipsAt(3, drivers) should knowAll(Set(Gossip1, Gossip2))
   }
+
+  private lazy val BusRoute1: BusRoute = BusRoute(Seq(1, 2, 3))
+  private lazy val BusRoute2: BusRoute = BusRoute(Seq(5, 4, 3))
+
+  private lazy val Gossip1: Gossip = Gossip(1)
+  private lazy val Gossip2: Gossip = Gossip(2)
+
+  private lazy val drivers = Seq(Driver(BusRoute1, Set(Gossip1)), Driver(BusRoute2, Set(Gossip2)))
+}
+
+trait DriverMatchers {
+  class DriverKnowsGossipsMatcher(gossips: Set[Gossip]) extends Matcher[Seq[Driver]] {
+    override def apply(left: Seq[Driver]): MatchResult = MatchResult(
+      matches = left.map(_.gossips).forall(_ == gossips),
+      rawFailureMessage = s"not all drivers $left know all gossips $gossips",
+      rawNegatedFailureMessage = "all drivers know all gossips")
+  }
+
+  def knowAll(gossips: Set[Gossip]) = new DriverKnowsGossipsMatcher(gossips)
 }
--- src/test/scala/es/eriktorr/katas/UnitSpec.scala
+++ src/test/scala/es/eriktorr/katas/UnitSpec.scala
@@ -3,4 +3,3 @@ package es.eriktorr.katas
 import org.scalatest._
 
 abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors
-
